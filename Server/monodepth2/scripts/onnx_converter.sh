46feb864f8d7b23c95df110c6277829ae1730044
vnminhhuy2001 <huyvnm@fpt.com.vn>
2023-10-29 12:03:49 +0700

    Add source monodepth2

---
+++ Server/monodepth2/scripts/onnx_converter.sh
@@ -0,0 +1,5 @@
+python3 ONNXconverter.py \
+        --load_weights_folder /workspace/source/mono2_ckpt \
+        --eval_mono\
+        --num_layers 101\
+        --data_path /workspace/source/data/rgb
